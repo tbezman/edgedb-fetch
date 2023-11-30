ebc85f48fe095fd2c8d936f1d7b5a1479b2ae6e4
Terence Bezman <terencebezman@gmail.com>
2023-11-30 18:23:38 -0500

    remove new post modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { FallbackCard, PostCard } from "@/app/PostCard";
-import { NewPostModal } from "./NewPostModal";
 import { edgeql } from "../../dist/manifest";
 import { Suspense } from "react";
 import { createClient } from "../../dbschema/edgeql-js";
@@ -20,7 +19,6 @@ export default async function Home() {
     <div className="py-4 px-4">
       <div className="flex items-center justify-between sticky top-4">
         <h1 className="text-2xl font-bold mb-2">Posts</h1>
-        <NewPostModal />
       </div>
 
       <ul className="list-inside space-y-4">
