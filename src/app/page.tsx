fffe7b240f2cf64fc67fbbd98874f3758977ca89
Terence Bezman <terencebezman@gmail.com>
2023-11-30 23:18:51 -0500

    clean up some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { FallbackCard, PostCard } from "@/app/PostCard";
 import { edgeql } from "../../dist/manifest";
-import { Suspense } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import { client } from "@/client";
 
 export default async function Home() {
@@ -15,9 +15,7 @@ export default async function Home() {
 
   return (
     <div className="py-4 px-4">
-      <div className="flex items-center justify-between sticky top-4">
-        <h1 className="text-2xl font-bold mb-2">Posts</h1>
-      </div>
+      <Title>Posts</Title>
 
       <ul className="list-inside space-y-4">
         {posts.map((post) => {
@@ -30,6 +28,14 @@ export default async function Home() {
           );
         })}
       </ul>
+    </div>
+  );
+}
+
+function Title({ children }: PropsWithChildren) {
+  return (
+    <div className="flex items-center justify-between sticky top-4">
+      <h1 className="text-2xl font-bold mb-2">{children}</h1>
     </div>
   );
 }
