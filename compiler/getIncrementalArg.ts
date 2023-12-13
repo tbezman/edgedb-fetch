3cccd29a32c0605a29d331af440d4c2bc056a778
Terence Bezman <terencebezman@gmail.com>
2023-12-12 22:11:47 -0500

    delete compiler and extension

--- compiler/getIncrementalArg.ts
+++
@@ -1,4 +0,0 @@
-let counter = 0;
-export function getIncrementalArg() {
-  return `arg${++counter}`;
-}
--- compiler/type.ts
+++
@@ -1,8 +0,0 @@
-import { CodeBlockWriter } from "ts-morph";
-import { TypeContext } from "../antlr/MyGrammarParser";
-
-export function writeType(writer: CodeBlockWriter, type: TypeContext) {
-  if (type.getText() === "string") {
-    writer.write("string");
-  }
-}
