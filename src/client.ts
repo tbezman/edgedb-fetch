65192a836b8268bedea699d4fa4a34e3db37beb2
Terence Bezman <terencebezman@gmail.com>
2023-12-19 22:07:32 -0500

    ensure client never gets recreated

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,16 @@
+import { Client } from "edgedb";
 import { createClient } from "../dbschema/edgeql-js";
 
-export const client = createClient();
+const edgedbClientSingleton = () => {
+  return createClient({ dsn: process.env.EDGEDB_DSN });
+};
+
+declare global {
+  var edgedb: undefined | ReturnType<typeof edgedbClientSingleton>;
+}
+
+const edgedb = globalThis.edgedb ?? edgedbClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.edgedb = edgedb;
+
+export { edgedb as client };
